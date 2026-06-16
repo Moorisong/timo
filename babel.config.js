3c1fefcb6c9bdcbc6dd3c8781e79a7ddd2967883
Kim SongHyeon <thiagoo@naver.com>
2026-06-16 11:34:25 +0900

    fix(config): separate babel preset for app run and jest tests

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  presets: ['@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  const isTest = process.env.NODE_ENV === 'test';
+  return {
+    presets: isTest ? ['@react-native/babel-preset'] : ['babel-preset-expo'],
+  };
 };
